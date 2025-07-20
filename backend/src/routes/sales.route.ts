4120d18ec3f7013cc7bd111e3ae1d2189a53eede
Long Yang <mrlongyang90@gmail.com>
2025-07-20 21:04:02 +0700

    DeleteSales to DeleteSale

--- backend/src/routes/sales.route.ts
+++ backend/src/routes/sales.route.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { getSales, createSales, deleteSales } from '../controllers/sales.controller';
+import { getSales, createSales, deleteSale } from '../controllers/sales.controller';
 
 const router = Router();
 
 router.get('/', getSales);
 router.post('/', createSales);
-router.delete('/:bill/:product', deleteSales);
+router.delete('/:bill/:product', deleteSale);
 
 
 export default router;
